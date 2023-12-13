3a16a48ed1a6695b6999e2f18aedf7f0deeca2ca
Révész Zsolt <revzsolt29@gmail.com>
2023-12-13 20:48:21 +0100

    DB update

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,24 +1,26 @@
-CREATE TABLE roles(
-       id serial PRIMARY KEY,
-       name varchar(255) UNIQUE NOT NULL
-);
-
 CREATE TABLE users (
        id serial PRIMARY KEY,
        email varchar(45) UNIQUE NOT NULL,
        password varchar(60),
        created_on timestamp(6) with time zone,
-       role int NOT NULL,
-       FOREIGN KEY (role)
-            REFERENCES roles (id)
+       role varchar(10) NOT NULL
     );
 
 CREATE TABLE cars (
        id serial PRIMARY KEY,
        published_on timestamp(6) with time zone,
-       make varchar(255),
-       model varchar(255),
+       make varchar(45),
+       model varchar(60),
        price integer,
+       body_type varchar(15),
+       gearbox varchar(15),
+       fuel_type varchar(15),
+       production_year integer,
+       colour varchar(30),
+       doors integer,
+       engine_size integer,
+       engine_power integer,
+       drivetrain varchar(15),
        owner integer NOT NULL,
        FOREIGN KEY (owner)
             REFERENCES users (id)
