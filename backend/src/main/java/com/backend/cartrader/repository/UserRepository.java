0ea58b266f08fd985a5f4a87763a552bd348f198
Révész Zsolt <revzsolt29@gmail.com>
2023-12-14 22:26:42 +0100

    Refact

--- backend/src/main/java/com/backend/cartrader/repository/UserRepository.java
+++ backend/src/main/java/com/backend/cartrader/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.backend.cartrader.repository;
 import com.backend.cartrader.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);
