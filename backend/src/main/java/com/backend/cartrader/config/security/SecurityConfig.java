f30d6d24b8240c292cbf9a67b5434f16c1b9bfee
Révész Zsolt <revzsolt29@gmail.com>
2024-02-15 10:40:04 +0100

    Removing commented lines

--- backend/src/main/java/com/backend/cartrader/config/security/SecurityConfig.java
+++ backend/src/main/java/com/backend/cartrader/config/security/SecurityConfig.java
@@ -73,31 +73,4 @@ public class SecurityConfig{
         return http.build();
     }
 
-    /*@Bean
-    public AuthenticationManager authenticationManager(
-            UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-        return new ProviderManager(authenticationProvider);
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-*/
 }
