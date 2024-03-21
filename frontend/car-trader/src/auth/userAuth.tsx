2bf0ad1a9b0fb68c4b040a63918cfa8306231bb5
Révész Zsolt <revzsolt29@gmail.com>
2024-03-21 12:27:43 +0100

    Unused file deleted

--- frontend/car-trader/src/auth/userAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { apiAuth} from "../api/apiActions2";
-
-export const authUser = (userEmail: string, userPassword: string) => {
-  const userData = {
-    email: userEmail,
-    password: userPassword,
-  };
-  return apiAuth(userData);
-};
