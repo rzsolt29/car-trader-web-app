0a93d9984da3697bef09911501e0021214468e57
Révész Zsolt <revzsolt29@gmail.com>
2024-03-21 12:23:51 +0100

    Refact

--- frontend/car-trader/src/pages/Register.tsx
+++ frontend/car-trader/src/pages/Register.tsx
@@ -11,24 +11,28 @@ const Register = () => {
       })
 
       
-      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setFormData(prevData => ({...prevData, [name]: value}))
-      }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const {name, value} = e.target;
+      setFormData(prevData => ({...prevData, [name]: value}))
+    }
 
     
-      const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
+          if (formData.password !== formData.confirm_password) {
+            throw new Error("Password and confirm password must be the same");
+          }
           const response = apiRegistrationRequest(formData);
           console.log(response);
+
         }catch (error) {
           console.error(error);
         }
       }
 
     
-      console.log(formData);
+    console.log(formData);
 
     return (
         <div className="register">
@@ -37,26 +41,25 @@ const Register = () => {
         <div>
           <label htmlFor="email">Email:</label> 
           <br />
-          <input type="email" name="email" id="email" onChange={handleChange} value={formData.email} required maxLength={45}/>
+          <input type="email" name="email" id="email" onChange={handleChange} value={formData.email} required maxLength={45} />
           <br />
         </div>
 
         <div>
           <label htmlFor="password">Password:</label> 
           <br />
-          <input type="password" name="password" id="password" onChange={handleChange} value={formData.password} required maxLength={60}/>
+          <input type="password" name="password" id="password" onChange={handleChange} value={formData.password} required maxLength={60} />
           <br />
         </div>
 
         <div>
           <label htmlFor="confirm_password">Confirm password:</label> 
           <br />
-          <input type="password" name="confirm_password" id="confirm_password" onChange={handleChange} value={formData.confirm_password} required maxLength={60} 
-          />
+          <input type="password" name="confirm_password" id="confirm_password" onChange={handleChange} value={formData.confirm_password} required maxLength={60} />
           <br />
         </div>
 
-        <button>Sign Up</button>
+        <button type="submit">Sign Up</button>
 
       </form>
     </div>
