3a0ae341a958933878e90f00c8759db4386f8247
Révész Zsolt <revzsolt29@gmail.com>
2024-07-29 11:28:44 +0200

    Refact

--- frontend/car-trader/src/utils/CarList.tsx
+++ frontend/car-trader/src/utils/CarList.tsx
@@ -26,7 +26,7 @@ export const CarList = (items: CarListProps) => {
                 <li className="tile" key={car.id}>
                     <span>{car.make}</span>
                     <div>{car.model}</div>
-                    { car.price != null ? <div>HUF {car.price}</div> : null}
+                    { car.price != null && <div>HUF {car.price}</div> }
                 </li>
             ))}
             </ul>
